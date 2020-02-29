c50156893504308707ad6abcb480b0c53834bdb4
iftikhar <49979983+iftikhar1986@users.noreply.github.com>
2020-02-29 12:38:56 +0300

    Example of ServletConfig and ServletContext

---
+++ app05/WEB-INF/classes/SecondServlet.java
@@ -0,0 +1,27 @@
+import javax.servlet.*;
+import java.io.*;
+import java.util.*;
+
+public class SecondServlet extends GenericServlet 
+{
+	public void service(ServletRequest request,ServletResponse response)throws ServletException,IOException 
+	{
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		ServletConfig config = getServletConfig();
+		ServletContext context = getServletContext();
+		    out.println("<html>");
+			out.println("<body>");
+			out.println("<h2>");
+
+			out.println("a-->"+context.getInitParameter("a")+"<br>");
+			out.println("b-->"+context.getInitParameter("b")+"<br>");
+			out.println("c-->"+config.getInitParameter("c")+"<br>");
+			out.println("d-->"+config.getInitParameter("d")+"<br>");
+			out.println("e-->"+config.getInitParameter("e")+"<br>");
+			out.println("f-->"+config.getInitParameter("f")+"<br>");
+			out.println("</h2></body>");
+			out.println("</html>");
+
+	}
+}
