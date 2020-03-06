e574d59d4f18d2a483e2d222dc6fec92d342437e
iftikhar <49979983+iftikhar1986@users.noreply.github.com>
2020-03-07 01:09:52 +0300

    Request Redirection By Using Hyper Links (Servlet Communication)

---
+++ app21/src/com/soft/servlet/RedirectServlet.java
@@ -0,0 +1,29 @@
+package com.soft.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hutch")
+public class RedirectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><body>");
+		out.println("<h3 align='center'>");
+		out.println("To get Services from Hutch<br>");
+		out.println("Click on<br>");
+		out.println("<a href='http://localhost:1010/vodafoneapp/welcome.html'>|CC@vodafone.com|</a>");
+		out.println("</h3></body></html>");
+
+
+	}
+
+}
