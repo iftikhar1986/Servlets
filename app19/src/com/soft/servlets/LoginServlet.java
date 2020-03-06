4b16d637f44afca788988c3268b657165b742a42
iftikhar <49979983+iftikhar1986@users.noreply.github.com>
2020-03-06 21:00:15 +0300

    Intergration of Wildfly and Eclipse

---
+++ app19/src/com/soft/servlets/LoginServlet.java
@@ -0,0 +1,39 @@
+package com.soft.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String uname = request.getParameter("uname");
+		String upwd = request.getParameter("upwd");
+		
+		out.println("<html><body>");
+		out.println("<h2 style='color:red;' align='center'>Soft Solutions</h2>");
+		out.println("<h3 style='color:blue;' align='center'>User Login Status</h2>");
+		out.println("<h1 style='color:blue;' align='center'>");
+		if (uname.equals("durga") && upwd.equals("durga")) {
+			out.println("User Login Success");
+		}else {
+			out.println("User Login failure");
+		}
+		
+		out.println("</h1>");
+		out.println("<h3 align='center'>");
+		out.println("<a href='./loginform.html'>|User Login Page|</a>");
+		out.println("</h3></body></html>");
+	}
+
+}
