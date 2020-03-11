92330c4c993cb67c3dbf264e4ae1562c8cac8e2c
iftikhar <49979983+iftikhar1986@users.noreply.github.com>
2020-03-11 03:04:57 +0300

    Request Dispatcher Include Machinasm

--- app23/src/com/soft/service/StudentServiceImpl.java
+++ app23/src/com/soft/service/StudentServiceImpl.java
@@ -8,29 +8,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.soft.beans.Student;
+import com.soft.factory.ConnectionFactory;
 
 public class StudentServiceImpl implements StudentService {
 	
-	Connection con ;
-	Statement st;
-	ResultSet rs ;
 	
-	
-
-	public StudentServiceImpl() {
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","durga");
-			st  = con.createStatement();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
 	
 	public void addStudent(Student std) {
 		
 		try {
+			Connection con = ConnectionFactory.getConnection();
+			Statement st = con.createStatement();
 			st.executeUpdate("insert into student values('"+std.getSid()+"','"+std.getSname()+"','"+std.getSaddr()+"')");
 			
 		} catch (Exception e) {
@@ -44,7 +32,10 @@ public class StudentServiceImpl implements StudentService {
 		List<Student> stdList = null;
 		
 		try {
-			rs = st.executeQuery("select * from student");
+			Connection con = ConnectionFactory.getConnection();
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select * from student");
+			
 			stdList = new ArrayList<Student>();
 			while (rs.next()) {
 				Student std = new Student();
